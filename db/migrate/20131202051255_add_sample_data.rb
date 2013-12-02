fc90cdbb8c973a63ab3d65b16eb51cc3df2b52dc
Eskil Heyn Olsen <eskil@eskil.org>
2013-12-01 22:00:55 -0800

    Fix migration that adds default data

--- db/migrate/20131202051255_add_sample_data.rb
+++ db/migrate/20131202051255_add_sample_data.rb
@@ -4,48 +4,48 @@ class AddSampleData < ActiveRecord::Migration
     Schedule.create(:activity => surf,
                     :price => Money.new(10000, "USD"),
                     :date_at => "2013-12-31",
-                    :time_at => "12:00",
+                    :time_at => Time.parse("12:00",).seconds_since_midnight,
                     :spots => 6)
     Schedule.create(:activity => surf,
                     :price => Money.new(10000, "USD"),
                     :date_at => "2013-12-24",
-                    :time_at => "11:00",
+                    :time_at => Time.parse("11:00",).seconds_since_midnight,
                     :spots => 6)
     Event.create(:activity => surf,
                  :date_at => "2013-12-31",
-                 :time_at => "12:00",
+                 :time_at => Time.parse("12:00",).seconds_since_midnight,
                  :spots => 4)
 
     scuba = Activity.create(:name => "Scuba", :vendor => "Joe Diver")
     Schedule.create(:activity => scuba,
                     :price => Money.new(6500, "USD"),
                     :date_at => "2013-12-24",
-                    :time_at => "08:00",
+                    :time_at => Time.parse("08:00",).seconds_since_midnight,
                     :spots => 2)
     Schedule.create(:activity => scuba,
                     :price => Money.new(6500, "USD"),
                     :on_mon => true,
                     :on_wed => true,
                     :on_fri => true,
-                    :time_at => "08:00",
+                    :time_at => Time.parse("08:00",).seconds_since_midnight,
                     :spots => 4)
     Schedule.create(:activity => scuba,
                     :price => Money.new(6500, "USD"),
                     :on_mon => true,
                     :on_wed => true,
                     :on_fri => true,
-                    :time_at => "18:00",
+                    :time_at => Time.parse("18:00",).seconds_since_midnight,
                     :spots => 2)
     Event.create(:activity => scuba,
                  :date_at => "2013-12-23",
-                 :time_at => "08:00",
+                 :time_at => Time.parse("08:00",).seconds_since_midnight,
                  :spots => 2)
 
     sail = Activity.create(:name => "Sailing", :vendor => "Robert Redford")
     Schedule.create(:activity => sail,
                     :price => Money.new(100000, 'USD'),
                     :date_at => "2013-12-22",
-                    :time_at => "10:00",
+                    :time_at => Time.parse("10:00",).seconds_since_midnight,
                     :spots => 1)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,13 +18,6 @@ ActiveRecord::Schema.define(version: 20131202051255) do
     t.string   "vendor"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "on_mon"
-    t.boolean  "on_tue"
-    t.boolean  "on_wed"
-    t.boolean  "on_thu"
-    t.boolean  "on_fri"
-    t.boolean  "on_sat"
-    t.boolean  "on_sun"
   end
 
   create_table "events", force: true do |t|
